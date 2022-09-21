717e4531d4a7c5655e123e110c7865406510b4a5
LucasRosa53 <lucasrosa223@hotmail.com>
2022-09-21 20:04:04 -0300

    exercicio 1 completo

--- fundamentos/secao-04-intro-javascript/secao-04-exercicio-dia/sricpt.js
+++ fundamentos/secao-04-intro-javascript/secao-04-exercicio-dia/sricpt.js
@@ -1,2 +1,22 @@
-const a = 5;
-const b = 10;
+const a = 10;
+const b = 15;
+
+let adição = a + b;
+console.log("resultado adição:", adição);
+
+let subtração = a - b;
+console.log("resultado subtração:", subtração);
+
+let multiplicação = a * b;
+console.log("resultado multiplicação:", multiplicação);
+
+let divisão = a / b;
+console.log("resultado divisão:", divisão);
+
+let módulo = a % b;
+console.log("resultado módulo:", módulo);
+
+
+
+
+
