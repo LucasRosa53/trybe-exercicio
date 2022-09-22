1faba243b60bd598b4ae18697dd63d90ff48d180
LucasRosa53 <lucasrosa223@hotmail.com>
2022-09-22 02:20:42 -0300

    exercicio 4 else/if

---
+++ fundamentos/secao-04-intro-javascript/secao-04-exercicio-dia/script4.js
@@ -0,0 +1,15 @@
+const a = 20;
+const x = 60;
+const y = 100;
+
+let somaDosAngulos = a + x + y;
+
+let todosAngulosPositivos = a > 0 && x > 0 && y > 0;
+
+if (somaDosAngulos === 180) {
+    console.log("verdadeiro");
+} else if (somaDosAngulos != 180) {
+console.log("falso");
+} else 
+console.log("Erro: ângulo inválido");
+
