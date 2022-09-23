89670e39cf1e2263489f48872e576d7ec12d616c
LucasRosa53 <lucasrosa223@hotmail.com>
2022-09-23 03:10:51 -0300

    doug melhor professor todos ex feitos

---
+++ fundamentos/secao-04-intro-javascript/secao-04-exercicio-4.2/script1.js
@@ -0,0 +1,100 @@
+
+// console.log(numbers);
+
+// let soma = 0;
+// let valorFinal = 27;
+// for (let index = 0; index < numbers.length; index += 1) {
+
+//     soma = soma + numbers[index] / 10;
+
+// }
+
+// if (valorFinal > 20) {
+//     console.log("valor maior que 20");
+// } else {
+//     console.log("valor menor que 20");
+
+// }
+
+
+
+
+// let maiorValor = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+
+//     if (numbers[index] > maiorValor) {
+//         maiorValor = numbers[index];
+//     }
+
+// }
+
+
+
+// let menorNumero = 100;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] < menorNumero) {
+//         menorNumero = numbers[index];
+//     }
+// }
+// console.log(menorNumero);
+
+// let resultado = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] % 2 !== 0) {
+//     resultado += 1;
+//     }
+// }
+// if (resultado === 0) {
+//     console.log("nenhum valor ímpar encontrado");
+// } else {
+//     console.log(resultado);
+// }
+
+
+
+
+// console.log(maiorValor);
+// console.log(soma); 
+
+
+
+
+
+let contador = [];
+
+divisor = 0;
+
+for (let index = 0; index <= 25; index++) {
+    // console.log(index);
+    contador.push(index);
+    
+    divisor = contador[contador] / 2;
+
+
+}
+console.log(divisor);
+
+for (let index = 0; index <= 25; index++) {
+    console.log(contador[index] / 2);
+
+}
+// console.log(contador);
+
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// soma = 0;
+// for (index = 0; index < numbers.length; index++) {
+    
+//         // console.log(numbers[index] + numbers[index]);
+//     soma = soma + numbers[index];
+
+// }
+// console.log(soma);
+
+
+
+
+
